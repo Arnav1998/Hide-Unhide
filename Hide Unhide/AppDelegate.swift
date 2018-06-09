c70961847b42be5e5d095354773a87d36bfdc259
Arnav Singhania <arnavsinghania98@gmail.com>
2018-06-09 11:27:05 +0530

    Initial Commit

---
+++ Hide Unhide/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hide Unhide
+//
+//  Created by ARNAV SINGHANIA on 6/9/18.
+//  Copyright © 2018 Kay Bee Development. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hide Unhide/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hide Unhide
+//
+//  Created by ARNAV SINGHANIA on 6/9/18.
+//  Copyright © 2018 Kay Bee Development. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
